a3b442c4631b8d5129b87ed7e82311727ccc8a3d
konya <konya@home>
2011-01-19 02:28:37 +0900

    bomberman: basic components

--- bomberman/main.py
+++ bomberman/main.py
@@ -40,9 +40,31 @@ class Stage:
 
         return str
 
-class StageRenderer:
+class Renderer:
+    def do_expose(self, cr):
+        pass
+
+class StageRenderer(Renderer):
     def __init__(self, stage):
         self.stage = stage
+        self.margin = (10, 10, 10, 10)
+
+    def do_expose(self, cr):
+        global game
+        cell_width = (game.screen[0] - self.margin[1] - self.margin[3]) / game.map[0]
+        cell_height = (game.screen[1] - self.margin[0] - self.margin[2]) / game.map[1]
+        cell_size = min(cell_width, cell_height)
+
+        start_x = lambda x: self.margin[3] + x * cell_size
+        start_y = lambda y: self.margin[0] + y * cell_size
+
+        for x in xrange(0, len(self.stage.map)):
+            for y in xrange(0, len(self.stage.map[0])):
+                cr.set_source_rgb(0.5, 0.5, 0.5)
+                cr.rectangle(start_x(x), start_y(y), cell_size-1, cell_size-1)
+                cr.fill()
+
+        pass
 
 fps_counters = [0 for i in range(0, 5)]
 fps_cur_counter = 0
@@ -62,7 +84,6 @@ def print_fps():
 
         print 'fps =', float(total_count) / 5
         fps_counters[fps_cur_counter] = 1   # reset
-
     else:
         fps_counters[fps_cur_counter] += 1
 
@@ -75,16 +96,22 @@ def do_tick():
     return
 
 def do_expose(widget, event):
-    global area
+    global area, renderers
     width = widget.allocation.width
     height = widget.allocation.height
     cr = area.window.cairo_create()    
+    for r in renderers:
+        r.do_expose(cr)
 
     return
 
 def do_timeout():
-    do_tick()
-    area.queue_draw()
+    try:
+        do_tick()
+        area.queue_draw()
+    except KeyboardInterrupt:
+        global game
+        game.quit = True
 
     return True
 
@@ -97,17 +124,22 @@ def do_key_press(widget, event):
     return True
 
 def main():
-    ''' game data '''
+    # game data
     global game
     game = Game()
 
     global stage
     stage = Stage(*game.map)
 
-    ''' game redering '''
-    global area
+    # game redering
+    global renderers
+    stageRenderer = StageRenderer(stage)
+    renderers = []
+    renderers.append(stageRenderer)
 
     # gtk init
+    global area
+
     window = gtk.Window()
     window.connect('destroy', gtk.main_quit)
     window.connect('key-press-event', do_key_press)
@@ -119,7 +151,6 @@ def main():
     window.add(area)
     window.show_all()
         
-    # setting fps
     gobject.timeout_add(1000/game.fps, do_timeout)
 
     gtk.main()
@@ -127,7 +158,6 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-
     except KeyboardInterrupt:
         global game
         game.quit = True
